2702417333029df2426d4439eaddef7688a2f0d5
Incfex <kuro@kuroa.me>
2021-02-23 11:32:03 +0800

    add files

---
+++ block_verify.py
@@ -0,0 +1,6 @@
+'''
+Verify the proof-of-work
+Verify the prev hash
+Validate the transaction in the block
+If all three checks pass, append this block to the node's instance of the blockchain, and continue work
+'''
