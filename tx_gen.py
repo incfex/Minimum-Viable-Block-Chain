09e8eafa319683f7723ef1066d19e7db0b9a57ea
Incfex <kuro@kuroa.me>
2021-02-24 08:05:46 +0800

    tx_gen and tx_con working

--- tx_gen.py
+++ tx_gen.py
@@ -4,62 +4,81 @@ from nacl.signing import SigningKey, VerifyKey
 from nacl.encoding import HexEncoder
 from hash_gen import hash_gen as hg
 
-"""
-Potential Improvements:
-  use list of keys instead of names
-"""
 
-aiwass = SigningKey.generate()
-abel = SigningKey.generate()
-adam = SigningKey.generate()
-cain = SigningKey.generate()
-eve = SigningKey.generate()
-isaac = SigningKey.generate()
-judas = SigningKey.generate()
-lilith = SigningKey.generate()
+def tx_gen(keys):
+  # Generate first transaction
+  input = []
+  # Give every key-pair 100 to spend
+  output = [{
+      "value": 100,
+      "pubkey": x.verify_key.encode(encoder=HexEncoder).decode('utf-8')
+  } for x in keys]
+  # Calculate hashes
+  sig = hg(input, output)
+  sig = sig.encode('utf-8')
+  sig = keys[0].sign(sig, encoder=HexEncoder).decode('utf-8')
+  number = hg(input, output, sig)
+  # Populate dictionary for tx0
+  tx0 = {"number": number, "input": input, "output": output, "sig": sig}
+  # Make tx0 the first transaction in the list
+  tx0_json = json.dumps([tx0])
+  # WRITE the json string to file
+  with open('transactions.json', "w") as f:
+    f.write(tx0_json)
 
-input = []
-output = [
-  {
-    "value": 100,
-    "pubkey": abel.verify_key.encode(encoder=HexEncoder).decode('utf-8')
-  },
-  {
-    "value": 100,
-    "pubkey": adam.verify_key.encode(encoder=HexEncoder).decode('utf-8')
-  },
-  {
-    "value": 100,
-    "pubkey": cain.verify_key.encode(encoder=HexEncoder).decode('utf-8')
-  },
-  {
-    "value": 100,
-    "pubkey": eve.verify_key.encode(encoder=HexEncoder).decode('utf-8')
-  },
-  {
-    "value": 100,
-    "pubkey": isaac.verify_key.encode(encoder=HexEncoder).decode('utf-8')
-  },
-  {
-    "value": 100,
-    "pubkey": judas.verify_key.encode(encoder=HexEncoder).decode('utf-8')
-  },
-  {
-    "value": 100,
-    "pubkey": lilith.verify_key.encode(encoder=HexEncoder).decode('utf-8')
+
+def tx_con(txn, rx, s_key):
+  '''
+  txn is the transaction number of input, have format of list
+
+  rx is the output, have format of tuple (value, receiver public key)
+    value is double,
+    public key is encoded by HexEncoder
+
+  s_key is the sender's private key
+  '''
+  # Read tx from files
+  with open('transactions.json', "r") as f:
+    txs = f.read()
+  txs_list = json.loads(txs)
+  input = []
+  for t in txn:
+    tx_dict = next(i for i in txs_list if i["number"] == t)
+    input_dict = {
+        "number":
+            t,
+        "output":
+            next(i for i in tx_dict["output"]
+                 if i["pubkey"] == s_key.verify_key.encode(
+                     encoder=HexEncoder).decode('utf-8'))
+    }
+    input.append(input_dict)
+
+  # Craft output
+  output=[]
+  for t in rx:
+    rx_dict = {
+      "value": t[0],
+      "pubkey": t[1]
+    }
+    output.append(rx_dict)
+
+  sig = hg(input, output)
+  sig = sig.encode('utf-8')
+  sig = s_key.sign(sig, encoder=HexEncoder).decode('utf-8')
+  number = hg(input,output,sig)
+  tx = {
+    "number": number,
+    "input": input,
+    "output": output,
+    "sig": sig
   }
-]
-#sig_str = (json.dumps(input)+json.dumps(output)).encode('utf-8')
-#sig = sha256(sig_str).hexdigest()
-sig = hg(input, output)
-number = hg(input, output, sig)
+  print(tx)
 
-tx_0 = {
-  "number": number,
-  "input": input,
-  "output": output,
-  "sig": sig
-}
-tx_0_json = json.dumps(tx_0)
-with open('transactions.json', "w") as f:
-  f.write(tx_0_json)
+
+def main():
+  return
+
+
+if __name__ == "__main__":
+  main()
