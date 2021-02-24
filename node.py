b3fb5472916c87732accd0ee203eaaacfe64bbb3
Incfex <kuro@kuroa.me>
2021-02-25 07:34:03 +0800

    block contruct draft

--- node.py
+++ node.py
@@ -19,26 +19,46 @@ def node(gb, utp_json):
 
   utp_json: unverified transactions pool in json format
   """
+  virgin = 1
   bc = [json.loads(gb)]
   utp_list = json.loads(utp_json)
   vtp_list = []
 
   # Special treatment for first transaction
-  tx_dict = utp_list.pop(0)
-  if(not tx_vfy(tx_dict, vtp_list, bc, 1)):
-    print("found invalid transaction")
-  print("tx passed")
-  vtp_list.append(tx_dict)
-  
-  # Normal transactions
-  while utp_list:
+  if virgin:
     tx_dict = utp_list.pop(0)
-    if(not tx_vfy(tx_dict, vtp_list, bc)):
+    if(not tx_vfy(tx_dict, vtp_list, bc, 1)):
       print("found invalid transaction")
-      continue
     print("tx passed")
     vtp_list.append(tx_dict)
+    virgin = 0
+  
+  # Normal transactions
+  else: 
+    while utp_list:
+      tx_dict = utp_list.pop(0)
+      if(not tx_vfy(tx_dict, vtp_list, bc)):
+        print("found invalid transaction")
+        continue
+      print("tx passed")
+      vtp_list.append(tx_dict)
 
+  # Construct block
+  tx = tx_dict["number"]
+  prev = bc[-1]["pow"]
+  pow_tar = "7ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+  pow_cur = pow_tar
+  nonce = 0
+  while pow_cur >= pow_tar:
+    nonce += 1
+    pow_cur = hg(tx,prev,nonce)
+    print(pow_cur)
+  block = {
+    "tx" = tx,
+    "prev" = prev,
+    "nonce" = nonce,
+    "pow" = pow_cur
+  }
 
   
 
